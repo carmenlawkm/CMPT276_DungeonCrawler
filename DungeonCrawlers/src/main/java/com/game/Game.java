73d0763e3f56445dd15bba1cd056dc2710c03c74
Carmen Law <carmenlaw08@gmail.com>
2021-03-04 22:56:20 -0800

    Updating implementation for window/ graphics

--- DungeonCrawlers/src/main/java/com/game/Game.java
+++ DungeonCrawlers/src/main/java/com/game/Game.java
@@ -1,7 +1,85 @@
 package main.java.com.game;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.concurrent.ExecutionException;
 
-public class Game {
-    public static void main(String args[]){
+public class Game implements Runnable{
+    private Window window;
+    public int width, height;
+    public String title;
+
+    private Thread thread1; //game runs on thread1
+    private boolean running = false;
+
+    private BufferStrategy bs;
+    private Graphics g;
+
+
+    public Game (int width, int height, String title){
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+
+    //starts the thread
+    public synchronized void start(){
+        if(running) return;
+
+        running = true;
+        thread1 = new Thread (this);
+        thread1.start();
 
     }
+
+    //stops the thread
+    public synchronized void stop(){
+        if(!running) return;
+
+        running = false;
+        try{
+            thread1.join();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //game loop's method
+    public void update(){
+
+    }
+
+    //game loop's method
+    public void render(){
+        //buffer strategies allow the computer to draw things on the screen
+        bs = window.getCanvas().getBufferStrategy();
+        if(bs == null){
+            window.getCanvas().createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+        bs.show();
+        g.dispose();
+    }
+
+    //initialize game graphics etc
+    private void init(){
+        window = new Window(width, height, title);
+
+    }
+
+
+    //game loop
+    public void run (){ //runnable's method - runs whenever we start our thread
+        init();
+
+        while(running){
+            update();//update all variables, position of objects..etc
+            render();//render (draw) everthing on the window ---- loop
+        }
+
+        stop();
+    }
+
+
 }
--- DungeonCrawlers/src/main/java/com/game/Window.java
+++ DungeonCrawlers/src/main/java/com/game/Window.java
@@ -1,18 +1,43 @@
 package main.java.com.game;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Window extends Canvas{
-    public Window(int width,int height, String title, Game game){
-        JFrame frame=new JFrame(title);
-        frame.setPreferredSize(new Dimension(width, height));
-        frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize(new Dimension(width, height));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+import javax.swing.JFrame;
+
+public class Window {
+    private JFrame frame;
+    private Canvas canvas;
+
+    private String title;
+    private int width, height;
+
+    public Window(int width,int height, String title){
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createWindow();
+    }
+
+    private void createWindow(){
+        frame = new JFrame(title);
+        //setting size for window
+        frame.setSize(width,height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //let game exit
         frame.setResizable(false);//cannot resize game
-        frame.setLocationRelativeTo(null);//sets location of window
-        frame.add(game);
+        frame.setLocationRelativeTo(null);//let window start in the middle
         frame.setVisible(true);
-        game.start();
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+
+        frame.add(canvas);
+        frame.pack();
+    }
+
+    public Canvas getCanvas(){
+        return canvas;
     }
 }
