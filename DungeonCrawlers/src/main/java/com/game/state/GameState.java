c4b19f532990d3a16297f88e746f25fd2326c2b2
Carmen Law <carmenlaw08@gmail.com>
2021-03-07 01:47:23 -0800

    Adding a state abstract class..

---
+++ DungeonCrawlers/src/main/java/com/game/state/GameState.java
@@ -0,0 +1,17 @@
+package main.java.com.game.state;
+
+import java.awt.*;
+
+public class GameState extends State{
+    public GameState(){
+
+    }
+
+    public void update() {
+
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
---
+++ DungeonCrawlers/src/main/java/com/game/state/State.java
@@ -0,0 +1,21 @@
+package main.java.com.game.state;
+
+import java.awt.*;
+
+public abstract class State {
+
+    //state manager
+    private static State currentState = null;
+
+    public static void setState(State state){
+        currentState = state;
+    }
+
+    public static State getState(){
+        return currentState;
+    }
+
+    public abstract void update();
+    public abstract void render (Graphics g);
+
+}
