623b92140b8340a37cd21fce3353ecd4c740fa4e
JinTao <jza237@sfu.ca>
2021-03-07 20:43:28 -0800

    added keyinput

---
+++ DungeonCrawlers/src/main/java/com/game/KeyInput.java
@@ -0,0 +1,34 @@
+package main.java.com.game;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class KeyInput implements KeyListener{
+    private boolean[] keys;
+    public boolean up, down, left, right;
+    public KeyInput(){
+        keys=new boolean[256];
+    }
+    public void update(){
+        up=keys[KeyEvent.VK_W];
+        down=keys[KeyEvent.VK_S];
+        left=keys[KeyEvent.VK_A];
+        right=keys[KeyEvent.VK_D];
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()]=true;
+
+    }
+    public void keyReleased(KeyEvent e){
+        keys[e.getKeyCode()]=false;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+}
