1007a4e1c286d9378dcbf4b6602aa720cf02f477
Carmen Law <carmenlaw08@gmail.com>
2021-03-08 03:04:53 -0800

    Updated pictures for every object

--- DungeonCrawlers/src/main/java/com/game/graphics/Assets.java
+++ DungeonCrawlers/src/main/java/com/game/graphics/Assets.java
@@ -4,12 +4,19 @@ import java.awt.image.BufferedImage;
 
 
 public class Assets {
-    //private static final int width = 32, height = 32;
-    public static BufferedImage player;
+
+    public static BufferedImage player, enemy, barrier, exit, floor, specialReward, regularReward, trap;
 
     //load in everything(material) in our game
     public static void initAssets(){
         player = ImageLoader.loadImage("/main/resources/mainCharacter.png");
+        enemy = ImageLoader.loadImage("/main/resources/enemy.png");
+        barrier = ImageLoader.loadImage("/main/resources/barrier.png");
+        exit = ImageLoader.loadImage("/main/resources/exit.png");
+        floor = ImageLoader.loadImage("/main/resources/floor.png");
+        specialReward = ImageLoader.loadImage("/main/resources/specialReward.png");
+        regularReward = ImageLoader.loadImage("/main/resources/regularReward.png");
+        trap = ImageLoader.loadImage("/main/resources/trap.png");
 
     }
 }
