f4a17ee0f35c20a6fe7c3fa7549b0f928172870f
JinTao <jza237@sfu.ca>
2021-03-11 20:54:23 -0800

    ensures main character does not go out of bounds

--- DungeonCrawlers/src/main/java/GameObjects/MainCharacter.java
+++ DungeonCrawlers/src/main/java/GameObjects/MainCharacter.java
@@ -1,5 +1,6 @@
 package GameObjects;
 
+import com.sun.tools.javac.Main;
 import state.Game;
 import graphics.*;
 
@@ -8,6 +9,7 @@ import java.awt.*;
 public class MainCharacter extends GameObject {
     private Point spawn;
     protected int score;
+    private static MainCharacter instance=null;
 
     //constructor
     public MainCharacter(Point location) {
@@ -15,22 +17,38 @@ public class MainCharacter extends GameObject {
         score = 0;
         this.image = Assets.player;
     }
+    public static MainCharacter getInstance(){
+
+        if(instance == null){
+            instance = new MainCharacter(new Point(100,100));
+        }
+        return instance;
+    }
+
 
     @Override
     public void update() {
         game.getKeyInput().update();
 
         if (game.getKeyInput().up) {
-            location.y -= 50;
+            if((location.y-50<=800) && (location.y-50>=0)){
+                location.y -= 50;
+            }
         }
         if (game.getKeyInput().down) {
-            location.y += 50;
+            if((location.y+50<=800) && (location.y+50>=0)){
+                location.y += 50;
+            }
         }
         if (game.getKeyInput().left) {
-            location.x -= 50;
+            if((location.x-50<=1200) && (location.x-50>=0)){
+                location.x -= 50;
+            }
         }
         if (game.getKeyInput().right) {
-            location.x += 50;
+            if((location.x+50<=1200) && (location.x+50>=0)){
+                location.x += 50;
+            }
         }
     }
 
