6ab80686ba0d38e19b01489d76d504ad2f3f16b7
Helen <thp5@sfu.ca>
2021-03-12 21:28:00 -0800

    Updating Enemy

--- DungeonCrawlers/src/main/java/GameObjects/Enemy.java
+++ DungeonCrawlers/src/main/java/GameObjects/Enemy.java
@@ -1,77 +1,46 @@
 package GameObjects;
 
-import com.sun.tools.javac.Main;
 import graphics.Assets;
 
 import java.awt.*;
 
-public class Enemy extends GameObject{
-    //MainCharacter player=MainCharacter.getInstance();
-    MainCharacter player=MainCharacter.getInstance();
+public class Enemy extends GameObject {
+    MainCharacter player = MainCharacter.getInstance();
     private int deductionValue;
-    private int stunned=0;
+
     public Enemy(Point location, ID id, int deductionValue) {
         super(location);
         this.deductionValue = deductionValue;
         this.image = Assets.enemy;
     }
 
-    void setDeductionValue(int value){
+    void setDeductionValue(int value) {
         deductionValue = value;
     }
-    //public stunned
 
     @Override
     public void update() {
         // enemy will prioritize moving in terms of left and right direction to
         // player then prioritizes moving up and down closer to player
-        int flag=0;
-        //int stunned=0;
-        int xtowardsplayer=0;
-        int ytowardsplayer=0;
-        if(location.x==player.getX() && location.y==player.getY()){
-            flag=1;
-            player.score=player.score-deductionValue;
+        int xTowardsPlayer = 0;
+        int yTowardsPlayer = 0;
+
+        if (location.x == player.getX() && location.y == player.getY()) {
+            player.score = player.score - deductionValue;
             System.out.println("enemy is in same spot as player");
-
-            stunned=1;
+        } else if (location.x < player.getX()) {
+            xTowardsPlayer = 40;
+            System.out.println("enemy is to the left of player");
+        } else if (location.x > player.getX()) {
+            xTowardsPlayer = -40;
+            System.out.println("enemy is to the right of player");
+        } else if (location.y < player.getY()) {
+            yTowardsPlayer = 40;
+            System.out.println("enemy is below the player");
+        } else if (location.y > player.getY()) {
+            yTowardsPlayer = -40;
+            System.out.println("enemy is above the player");
         }
-        while(flag==0 && stunned==0) {
-            xtowardsplayer=0;
-            ytowardsplayer=0;
-            if (location.x == player.getX() && location.y==player.getY()) {
-                xtowardsplayer = 0;
-                ytowardsplayer = 0;
-                flag=1;
-                System.out.println("enemy is to the left of player");
-                break;
-            }
-            else if (location.x > player.getX()) {
-                xtowardsplayer = -40;
-                flag=1;
-                System.out.println("enemy is to the right of player");
-                break;
-            }
-            else if (location.y < player.getY()) {
-                ytowardsplayer = 40;
-                flag=1;
-                System.out.println("enemy is above the player");
-                break;
-            }
-            else if (location.y > player.getY()) {
-                ytowardsplayer = -40;
-                System.out.println("enemy is below the player");
-                break;
-            }
-            else{
-                xtowardsplayer=0;
-                ytowardsplayer=0;
-                System.out.println("did this trigger");
-                break;
-            }
-
-
-        }
-        setLocation(location.x+xtowardsplayer, location.y + ytowardsplayer);
+        setLocation(location.x + xTowardsPlayer, location.y + yTowardsPlayer);
     }
 }
