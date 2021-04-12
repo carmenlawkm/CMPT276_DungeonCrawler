6735cc1d95d175ea141810c19739cbef7af1fe59
kmlaw <carmen_law@sfu.ca>
2021-04-12 06:58:55 -0700

    adding input test files

---
+++ DungeonCrawlers/src/test/java/input/KeyInputTest.java
@@ -0,0 +1,28 @@
+package input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeyInputTest {
+
+    @Test
+    void setKeyPressed() {
+    }
+
+    @Test
+    void keyPressed() {
+    }
+
+    @Test
+    void keyReleased() {
+    }
+
+    @Test
+    void keyTyped() {
+    }
+
+    @Test
+    void getKeyCode() {
+    }
+}
---
+++ DungeonCrawlers/src/test/java/input/MouseInputTest.java
@@ -0,0 +1,40 @@
+package input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MouseInputTest {
+
+    @Test
+    void isLeftPressed() {
+    }
+
+    @Test
+    void mouseClicked() {
+    }
+
+    @Test
+    void mousePressed() {
+    }
+
+    @Test
+    void mouseReleased() {
+    }
+
+    @Test
+    void mouseEntered() {
+    }
+
+    @Test
+    void mouseExited() {
+    }
+
+    @Test
+    void mouseDragged() {
+    }
+
+    @Test
+    void mouseMoved() {
+    }
+}
