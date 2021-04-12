c90e67e881f477ff02eb52881fda2320522d25c4
kmlaw <carmen_law@sfu.ca>
2021-04-12 06:35:59 -0700

    finished world test

--- DungeonCrawlers/src/test/java/World/WorldTest.java
+++ DungeonCrawlers/src/test/java/World/WorldTest.java
@@ -1,13 +1,14 @@
 package World;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorldTest {
-    //not starting with before each for now since world is not necessary in every tests
 
     @Test
-    void loadFile() {
+    void shouldLoadFile() {
         assertEquals("15 10\n" +
                 "0 0\n" +
                 "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n" +
@@ -19,32 +20,66 @@ class WorldTest {
                 "1 0 1 1 0 1 0 0 0 0 0 0 0 0 1\n" +
                 "1 0 0 1 0 0 0 0 0 1 0 5 1 0 2\n" +
                 "1 0 0 0 0 0 0 0 0 1 0 0 0 0 1\n" +
-                "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n", World.loadFile("src/main/resources/fileTest.txt"));
+                "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n", World.loadFile("src/test/resources/fileTest.txt"));
     }
 
     @Test
-    void loadWorld(){
+    void shouldConvertStringToInt() {
+        //should correctly convert string into integer
+        assertEquals(10,World.stringToInt("10"));
+    }
+
+
+    //testing constructor & loadWorld
+    @Test
+    void shouldCreateWorld(){
         //loadTest passed
-        //World world = new World("src/main/resources/fileTest.txt");
-        //what should I be asserting? I won't be allowed to assert with getWidth, getHeight since they haven't been tested
-    }
-
-    @Test
-    void getWidth() {
-        World world = new World("src/main/resources/fileTest.txt");
+        World world = new World("src/test/resources/fileTest.txt");
+        //check width, height has been assigned correctly
         assertEquals(15, world.getWidth());
-    }
-
-    @Test
-    void getHeight() {
-        World world = new World("src/main/resources/fileTest.txt");
         assertEquals(10, world.getHeight());
+        //check spawnX, spawn Y has been assigned correctly
+        assertEquals(0, world.getSpawnX());
+        assertEquals(0, world.getSpawnY());
+        //check tileArray has been transferred correctly
+        int [][] testTilesID= {
+                {1, 3, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+                {1, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 0, 1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0, 5, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 2, 1, 1}
+        };
+
+        int [][] tilesID = world.getTilesID();
+
+        for(int j = 0; j<world.getHeight(); j++) {
+            for (int i = 0; i < world.getWidth(); i++) {
+                assertEquals(testTilesID[i][j], tilesID[i][j]);
+            }
+        }
     }
 
-
+    @Test
+    void shouldGetOriginalTile() {
+        World world = new World("src/main/resources/fileTest.txt");
+        //case: in bound and not null should return the corresponding Tile type with correct id
+        int x = 0;
+        int y = 0;
+        assertEquals(Tile.barrierTile, world.getTile(x,y));
+    }
 
     @Test
-    void getTile() {
+    void shouldGetDefaultTile(){
         //need to test that loadWorld and loadFile correctly loads path first
         World world = new World("src/main/resources/fileTest.txt");
         int x, y = 0;
@@ -64,18 +99,8 @@ class WorldTest {
         y = 7;
         x = 11;
         assertEquals(Tile.floorTile, world.getTile(x,y));
-
-        //case 2: in bound and not null should return the corresponding Tile type with correct id
-        x = 0;
-        y = 0;
-        assertEquals(Tile.barrierTile, world.getTile(x,y));
     }
 
-    @Test
-    void stringToInt() {
-        //should correctly convert string into integer
-        assertEquals(10,World.stringToInt("10"));
-    }
 
 
 }
